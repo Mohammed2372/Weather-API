294d2b24f6a0bead11d4b46acb363e52701bb5f4
Mohammed2372 <mohammedmostafa2372002@gmail.com>
2025-11-16 18:01:42 +0200

    handle weather ulr and page with statis data

--- weather/urls.py
+++ weather/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .views import weather_api
+
 urlpatterns = [
-    
+    path("<str:city>/", weather_api, name="weather"),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+def weather_api(request, city):
+    data = {
+        "city": city,
+        "temperature": 15,
+        "unit": "celsius",
+        "description": "Mostly cloudy",
+        "api_source": "local_stub (Step 1)",
+    }
+    return JsonResponse(data)
